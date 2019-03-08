7f5aad88cc1b53cb9706f970fff0061d1422cf65
Christopher Kosik <37384678+Kosik-Chris@users.noreply.github.com>
2019-03-08 12:20:09 -0500

    Update RandomData.js

--- Test/RandomData.js
+++ Test/RandomData.js
@@ -1,9 +1,14 @@
-  exports.getRandomInt = function(min,max){
+/***
+Fills 16 sensor output channels with random data. Each returned array of
+16 values serves as one sensor point per time displayed, where time is defined
+in a 250ms update speed.
+**/
+  exports.getRandomInt = function(min,max) {
 
     var randomArray = [];
-    for(var i = 0; i <16; i++){
+    for (var i = 0; i < 16; i++) {
       randomArray[i] = Math.floor(Math.random() * (max - min + 1)) + min;
     }
     //16 random numbers
     return randomArray;
-  }
+  };
