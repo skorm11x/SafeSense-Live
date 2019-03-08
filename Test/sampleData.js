aa60c89f00b8fcdf02424b36059524d1110e7d7e
Christopher Kosik <37384678+Kosik-Chris@users.noreply.github.com>
2019-03-08 12:20:28 -0500

    Update sampleData.js

--- Test/sampleData.js
+++ Test/sampleData.js
@@ -1,3 +1,9 @@
+/***
+Fills 16 output channels with stored database data. Phase offset starts at index
+0.Each returned array of
+16 values serves as one sensor point per time displayed, where time is defined
+in a 250ms update speed.
+**/
 exports.getSampleTest = function(){
 
-}
+};
